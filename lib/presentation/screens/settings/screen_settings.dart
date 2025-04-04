39bc56b928a5ba4535cf5398ca5ade19c85b79f0
Soldate <soldate@live.com>
2025-04-04 12:35:41 +0800

    ADD: 增加权限

--- lib/presentation/screens/settings/screen_settings.dart
+++ lib/presentation/screens/settings/screen_settings.dart
@@ -3,20 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:logger/logger.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:logger/logger.dart';
 
 import '../../../bloc/theme/theme_bloc.dart';
 import '../../../bloc/theme/theme_events.dart';
 import '../../../bloc/theme/theme_states.dart';
 import '../../theme/app_themes.dart';
 import '../../widgets/custom_list_tile.dart';
-
-var logger = Logger(
-  filter: null, // Use the default LogFilter (-> only log in debug mode)
-  printer: PrettyPrinter(), // Use the PrettyPrinter to format and print log
-  output: null, // Use the default LogOutput (-> send everything to console)
-);
 
 var logger = Logger(
   filter: null, // Use the default LogFilter (-> only log in debug mode)
